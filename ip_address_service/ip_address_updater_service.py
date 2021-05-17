0e818802898db1778ec879dd547e737102ecfe5b
Dallin Miner <keyofd.miner@gmail.com>
2021-05-17 14:41:05 -0600

    Switch updater from pip to git

--- ip_address_service/ip_address_updater_service.py
+++ ip_address_service/ip_address_updater_service.py
@@ -1,28 +1,6 @@
 import subprocess
 import os
 
-RELEASE = "master"  # default release
-REPO_NAME = "IP_address_service"
-UPDATE_CMD = (  # base command
-    'pip3 install --src="%s" --upgrade -e '
-    "git+https://github.com/3D-Printing-for-Microfluidics/IP_address_service.git@%s#egg=%s"
-)
-
-os.chdir("../..")
-src_dir = os.getcwd()
-release = RELEASE
-commit = None
-
-release = "origin/" + release
-cmd = UPDATE_CMD % (src_dir, release, REPO_NAME)
-try:
-    subprocess.run(cmd, shell=True)
-except OSError:
-    print(cmd)
-    print("Update failed")
-    pass
-
-# os.chdir("ip-address-service")
-# subprocess.run("rm setup.py", shell=True)
-# subprocess.run("rm -r UNKNOWN.egg-info", shell=True)
-# subprocess.run("pip3 uninstall -y unknown", shell=True)
+subprocess.run("chmod 600 ../deploy", shell=True)
+subprocess.run("ssh-agent sh -c 'ssh-add ../deploy; git fetch'", shell=True)
+subprocess.run("ssh-agent sh -c 'ssh-add ../deploy; git pull'", shell=True)
