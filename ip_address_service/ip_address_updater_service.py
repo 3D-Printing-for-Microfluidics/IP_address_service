4d9b7657d993090a9aec67b30bfa3a074eecc444
Dallin Miner <keyofd.miner@gmail.com>
2021-05-17 14:44:18 -0600

    Switch from pull to reset --hard

--- ip_address_service/ip_address_updater_service.py
+++ ip_address_service/ip_address_updater_service.py
@@ -3,4 +3,6 @@ import os
 
 subprocess.run("chmod 600 ../deploy", shell=True)
 subprocess.run("ssh-agent sh -c 'ssh-add ../deploy; git fetch'", shell=True)
-subprocess.run("ssh-agent sh -c 'ssh-add ../deploy; git pull'", shell=True)
+subprocess.run(
+    "ssh-agent sh -c 'ssh-add ../deploy; git reset --hard origin/master'", shell=True
+)
